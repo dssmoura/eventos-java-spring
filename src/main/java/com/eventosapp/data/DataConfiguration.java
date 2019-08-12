35748cf7186f75664d942dc8280807169451fd05
danilo.moura <danilo.moura@luizalabs.com>
2019-08-12 07:40:02 -0300

    bd

--- src/main/java/com/eventosapp/data/DataConfiguration.java
+++ src/main/java/com/eventosapp/data/DataConfiguration.java
@@ -1,8 +1,12 @@
 package com.eventosapp.data;
 
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -15,24 +19,41 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 @Profile("dev")
 public class DataConfiguration {
 
-	@Bean
-    public DataSource dataSource(){
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
-        dataSource.setUsername("root");
-        dataSource.setPassword("123");
-        return dataSource;
-    }
+//	@Bean
+//    public DataSource dataSource(){
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+//        dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp");
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("123");
+//        return dataSource;
+//    }
+//	
+//	@Bean
+//	public JpaVendorAdapter jpaVendorAdapter(){
+//		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+//		adapter.setDatabase(Database.MYSQL);
+//		adapter.setShowSql(true);
+//		adapter.setGenerateDdl(true);
+//		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+//		adapter.setPrepareConnection(true);
+//		return adapter;
+//	}
+	
 	
 	@Bean
-	public JpaVendorAdapter jpaVendorAdapter(){
-		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
-		adapter.setDatabase(Database.MYSQL);
-		adapter.setShowSql(true);
-		adapter.setGenerateDdl(true);
-		adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
-		adapter.setPrepareConnection(true);
-		return adapter;
-	}
+    public BasicDataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        BasicDataSource basicDataSource = new BasicDataSource();
+        basicDataSource.setUrl(dbUrl);
+        basicDataSource.setUsername(username);
+        basicDataSource.setPassword(password);
+
+        return basicDataSource;
+    }
 }
